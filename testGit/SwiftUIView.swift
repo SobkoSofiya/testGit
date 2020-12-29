d4c6b2127dea434c9b13f4dfecb180a372bd3f1b
Sergei <Klimovich>
2020-12-29 16:50:06 +0300

    test2 commit

--- testGit/SwiftUIView.swift
+++ testGit/SwiftUIView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct SwiftUIView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Hello, world!")
+                .padding()
+            Text("Hello, world!")
+                    .padding()
+            Text("Hello, world!")
+                .padding()
+        }
     }
 }
 
