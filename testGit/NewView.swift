f4caca55f75ba961188d26d571bf72ee5feb03b5
Sergei <Klimovich>
2020-12-29 16:36:55 +0300

    test1 commit

---
+++ testGit/NewView.swift
@@ -0,0 +1,20 @@
+//
+//  NewView.swift
+//  testGit
+//
+//  Created by Admin on 29.12.2020.
+//
+
+import SwiftUI
+
+struct NewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewView()
+    }
+}
